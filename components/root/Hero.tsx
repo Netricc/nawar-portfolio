d5bb624d6fb47d5f6a33a1a95eda02ca7deebb8e
netric <nawabsn911@gmail.com>
2025-03-13 10:02:46 +0100

    Make Hero Section Animation with Gsap

--- components/root/Hero.tsx
+++ components/root/Hero.tsx
@@ -1,14 +1,45 @@
+"use client";
+
+import gsap from "gsap";
+import { useEffect } from "react";
+
 const Hero = () => {
+  useEffect(() => {
+    gsap.to("#hero-h1", {
+      delay: 3.4,
+      opacity: 1,
+    });
+    gsap.to("#hero-h2", {
+      delay: 3,
+      opacity: 1,
+      y: 0,
+    });
+
+    gsap.to("#hero-p", {
+      delay: 3.2,
+      opacity: 1,
+      y: 0,
+    });
+  }, []);
   return (
     <section className="w-full min-h-screen  pt-[94px] pb-[118px] max-lg:pt-[78px] max-lg:pb-[326px] max-sm:pt-[80px] max-sm:pb-[176px]">
       <div className="container-max w-full">
-        <h1 className="font-dm-sans text-secondary text-[58px] max-sm:text-[36px] font-extralight italic">
+        <h1
+          id="hero-h1"
+          className="font-dm-sans opacity-0 text-secondary text-[58px] max-sm:text-[36px] font-extralight italic"
+        >
           Nawar
         </h1>
-        <h2 className="font-dm-sans text-secondary text-[58px] max-sm:text-[36px]">
+        <h2
+          id="hero-h2"
+          className="font-dm-sans opacity-0 translate-y-4 text-secondary text-[58px] max-sm:text-[36px]"
+        >
           Designer & Front-end Developer
         </h2>
-        <p className="mt-[60px] text-[20px] font-inter text-gray w-[760px] max-lg:w-[502px] max-sm:w-full">
+        <p
+          id="hero-p"
+          className="mt-[60px] opacity-0 translate-y-4 text-[20px] font-inter text-gray w-[760px] max-lg:w-[502px] max-sm:w-full"
+        >
           I am Front-End Developer with a focus on animation and design,
           crafting interactive and visually stunning digital experiences.
         </p>
